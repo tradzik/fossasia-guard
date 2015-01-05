a9e9f915730af04d57bcaebc4b3e3bdbec8e76fa
root <root@TRServer.(none)>
2015-01-05 16:21:13 -0500

    Ran autopep8; code cleanup

--- AdminBot/LoggingMain.py
+++ AdminBot/LoggingMain.py
@@ -1,14 +1,16 @@
 from twisted.python import log
 import time
 
+
 class LoggingMain:
-	def __init__(self, file):
-		self.file = file
 
-	def log(self, msg):
-		timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
-		self.file.write('[%s]: %s\n' % (timestamp, msg))
-		self.file.flush()
+    def __init__(self, file):
+        self.file = file
 
-	def close(self):
-		self.file.close()
+    def log(self, msg):
+        timestamp = time.strftime("[%H:%M:%S]", time.localtime(time.time()))
+        self.file.write('[%s]: %s\n' % (timestamp, msg))
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
